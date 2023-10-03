0b73ef7b115e9eca052636cdd0c25240c7ae6c20
Théo Barreto Silva <barretotheo25@gmail.com>
2023-10-03 17:08:50 -0300

    upload

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return(
         <S.Header>
             <LogoTexto />
-            <S.ImgIconMenu src='../../src/assets/svg/icon_menu.svg' />
+            <S.ImgIconMenu src='https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/7c76764445c72523c6b4eb6a170e55d59d26bead/src/assets/svg/icon_menu.svg' />
             <S.Navegacao>
                 <S.LinkNav to={'inicio'}>Início</S.LinkNav>
                 <S.LinkNav to={'espaco'}>Espaço</S.LinkNav>
