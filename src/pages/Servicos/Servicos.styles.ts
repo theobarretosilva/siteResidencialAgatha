b2a36d91d39f109c42a6babbbed2e5bc16c16ff4
Théo Barreto Silva <barretotheo25@gmail.com>
2023-10-04 22:37:47 -0300

    terminando pagina de servicos

---
+++ src/pages/Servicos/Servicos.styles.ts
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+export const DivImgInicial = styled.div`
+    width: 100%;
+    height: 50vh;
+    background-image: url('../../src/assets/img/img_pagina_servicos.png');
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    object-fit: cover;
+`
+
+export const TituloDivImgInicial = styled.h1`
+    color: #FFF;
+    text-align: center;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 4rem;
+    font-style: normal;
+    font-weight: 600;
+    margin-top: 16vh;
+`
+
+export const DivTextoInicial = styled.div`
+    width: 100%;
+    height: 23vh;
+    border-radius: 1.25rem;
+    background: linear-gradient(193deg, #6A6A6A 9.58%, rgba(192, 192, 192, 0.50) 90.42%);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 7vh;
+    margin-bottom: 7vh;
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+`
+
+export const TextoInicial = styled.p`
+    color: #000;
+    text-align: center;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.3rem;
+    font-style: normal;
+    font-weight: 500;
+    max-width: 98%;
+`
+
+export const SectionBoxs = styled.section`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-bottom: 2vh;
+`
+
+export const DivBox = styled.div`
+    background-color: white;
+    border-radius: 0.625rem;
+    width: 24vw;
+    height: 50vh;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    margin-bottom: 5vh;
+`
+
+export const DivCorDentroBox = styled.div`
+    border-radius: 0.3125rem;
+    margin-top: 1vh;
+    margin-left: 1vh;
+    margin-right: 1vh;
+    height: 23vh;
+    text-align: start;
+    padding-left: 0.4vw;
+`
+
+export const NumeroDentroBoxCor = styled.p`
+    color: #FFF;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.75rem;
+    font-style: normal;
+    font-weight: 400;
+    margin-top: 1vh;
+`
+
+export const TituloDentroBoxCor = styled.h1`
+    color: #FFF;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.4rem;
+    font-style: normal;
+    font-weight: 500;
+`
+
+export const PDescBox = styled.p`
+    color: #000;
+    font-family: 'Inter', sans-serif;
+    font-size: 1.05rem;
+    font-style: normal;
+    font-weight: 400;
+    text-align: start;
+    margin-left: 1vh;
+    margin-right: 1vh;
+`
--- src/pages/Servicos/Servicos.tsx
+++ src/pages/Servicos/Servicos.tsx
@@ -1,5 +1,70 @@
+import { DivMargem } from '../../components/DivMargem/DivMargem'
+import * as S from './Servicos.styles'
+
 export function Servicos() {
+    const servicosData = [
+        {
+            cor: '#3888FF',
+            numero: '01',
+            titulo: 'Cuidados de enfermagem personalizados',
+            descricao: 'Oferecemos cuidados de enfermagem personalizados, adaptados às suas necessidades individuais. Nossa equipe de enfermagem é altamente treinada e dedicada a garantir que você receba os cuidados de saúde de que precisa.'
+        },
+        {
+            cor: '#20AA68',
+            numero: '02',
+            titulo: 'Atividades recreativas e sociais',
+            descricao: 'Promovemos uma variedade de atividades recreativas e sociais para manter sua mente e corpo ativos e envolvidos. De exercícios a passeios culturais, há sempre algo emocionante acontecendo em nosso residencial.'
+        },
+        {
+            cor: '#009C82',
+            numero: '03',
+            titulo: 'Supervisão médica geriátrica',
+            descricao: 'Contamos com uma equipe médica experiente e qualificada, especializada em geriatria, que compreende as complexidades do envelhecimento e das condições de saúde comuns nesta fase da vida.'
+        },
+        {
+            cor: '#FF9C41',
+            numero: '04',
+            titulo: 'Refeições deliciosas e nutritivas',
+            descricao: 'Nossa equipe de cozinha prepara refeições deliciosas, nutritivas e personalizadas, levando em consideração suas preferências alimentares e necessidades dietéticas.'
+        },
+        {
+            cor: '#FF7979',
+            numero: '05',
+            titulo: 'Segurança e conforto',
+            descricao: 'Criamos um ambiente seguro e acolhedor, adaptando nossas instalações para minimizar riscos e prevenir acidentes, permitindo que você viva com independência e tranquilidade.'
+        },
+        {
+            cor: '#A753A3',
+            numero: '06',
+            titulo: 'Assistência personalizada',
+            descricao: 'Nossa equipe está disponível para atender às suas necessidades individuais e garantir que você receba a atenção e o cuidado que merece.'
+        }
+    ]
+
     return(
-        <h1>TELA DE SERVIÇOS</h1>
+        <>
+            <S.DivImgInicial>
+                <S.TituloDivImgInicial>Nossos Serviços, Sua Tranquilidade</S.TituloDivImgInicial>
+            </S.DivImgInicial>
+            <DivMargem>
+                <S.DivTextoInicial>
+                    <S.TextoInicial>
+                        No nosso residencial, abraçamos uma abordagem holística para o cuidado dos idosos. Nossa missão é proporcionar um ambiente acolhedor e amoroso para idosos semi-dependentes ou dependentes, bem como aqueles que enfrentam desafios de saúde crônicos, como a doença de Alzheimer, derrame, doença de Parkinson e outras condições relacionadas.
+                    </S.TextoInicial>
+                </S.DivTextoInicial>
+                <S.SectionBoxs>
+                    {servicosData.map((servico, index) => (
+                        <S.DivBox key={index}>
+                           <S.DivCorDentroBox style={{backgroundColor: servico.cor}}>
+                               <S.NumeroDentroBoxCor>{servico.numero}</S.NumeroDentroBoxCor>
+                               <S.TituloDentroBoxCor>{servico.titulo}</S.TituloDentroBoxCor>
+                           </S.DivCorDentroBox>
+                           <S.PDescBox>{servico.descricao}</S.PDescBox>
+                       </S.DivBox> 
+                    ))}
+                    
+                </S.SectionBoxs>
+            </DivMargem>
+        </>
     )
 }
