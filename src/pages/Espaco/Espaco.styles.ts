627b2faf31ce097e2630dae59ce8143d92366f45
Théo Barreto Silva <barretotheo25@gmail.com>
2023-10-06 00:06:25 -0300

    fazendo tela de espaco

--- src/pages/Espaco/Espaco.styles.ts
+++ src/pages/Espaco/Espaco.styles.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const DivImgInicial = styled.div`
     width: 100%;
     height: 40vh;
-    background-image: url('https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/img_pagina_espaco.jpg?raw=true');
+    background-image: url('https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/img_pagina_espaco.png?raw=true');
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -20,4 +20,40 @@ export const TituloDivImgInicial = styled.h1`
     font-style: normal;
     font-weight: 600;
     margin-top: 7vh;
+`
+
+export const TextoInicial = styled.p`
+    color: #000;
+    text-align: center;
+    font-family: 'Montserrat', sans-serif;
+    font-size: 1.3rem;
+    font-style: normal;
+    font-weight: 500;
+    margin-top: 6vh;
+    margin-bottom: 0;
+`
+
+export const TextoSection = styled.h1`
+    color: black;
+    font-size: 3.1rem;
+    font-style: normal;
+    font-weight: 600;
+    font-family: 'Montserrat', sans-serif;
+    text-align: center;
+    margin-top: 0;
+`
+
+export const FaixaAcomodacoes = styled.div`
+    width: 99.7vw;
+    height: 50vh;
+    background-image: url('../../src/assets/img/faixa_torta.png');
+    object-fit: cover;
+`
+
+export const TituloAcomodacao = styled.h1`
+    color: #FFF;
+    font-size: 2.18rem;
+    font-style: normal;
+    font-weight: 500;
+
 `
--- src/pages/Espaco/Espaco.tsx
+++ src/pages/Espaco/Espaco.tsx
@@ -1,3 +1,5 @@
+import { DivMargem } from '../../components/DivMargem/DivMargem'
+import { Separador } from '../../components/Separador/Separador'
 import * as S from './Espaco.styles'
 
 export function Espaco() {
@@ -6,6 +8,16 @@ export function Espaco() {
             <S.DivImgInicial>
                 <S.TituloDivImgInicial>Nossa Estrutura Cuidadosamente Planejada</S.TituloDivImgInicial>
             </S.DivImgInicial>
+            <DivMargem>
+                <S.TextoInicial>
+                    Nossos espaços foram cuidadosamente projetadas para proporcionar conforto e tranquilidade. Cada espaço é pensado para atender às necessidades individuais dos nossos residentes, oferecendo um ambiente acolhedor e seguro.
+                </S.TextoInicial>
+                <Separador />
+            </DivMargem>
+            <S.TextoSection>Nossas acomodações</S.TextoSection>
+            <S.FaixaAcomodacoes>
+                <S.TituloAcomodacao>Quarto Ágata Azul</S.TituloAcomodacao>
+            </S.FaixaAcomodacoes>
         </>
     )
 }
