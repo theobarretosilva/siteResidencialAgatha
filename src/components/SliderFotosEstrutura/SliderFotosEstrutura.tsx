bb0af4096fb2fa95ff0e9dbc7f2dd0a7119e13b6
Théo Barreto Silva <barretotheo25@gmail.com>
2023-12-05 10:03:18 -0300

    upload fotos

--- src/components/SliderFotosEstrutura/SliderFotosEstrutura.tsx
+++ src/components/SliderFotosEstrutura/SliderFotosEstrutura.tsx
@@ -8,7 +8,7 @@ import './SliderFotosEstrutura.css'
 export const SliderFotosEstrutura = () => {
     return(
         <>
-            <h1 className="titulo">Sala de televisão</h1>
+            <h1 className="titulo">Sala de estar</h1>
             <Swiper
                 slidesPerView={3}
                 modules={[Autoplay]}
@@ -18,7 +18,7 @@ export const SliderFotosEstrutura = () => {
                     disableOnInteraction: false,
                 }}
             >
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1600566753104-685f4f24cb4d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8dHYlMjByb29tfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60' /></SwiperSlide>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/salaDeEstar/salaDeEstar1.jpg?raw=true' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/flagged/photo-1572609239482-d3a83f976aa0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1461151304267-38535e780c79?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1633&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1611094016919-36b65678f3d6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' /></SwiperSlide>
@@ -33,7 +33,7 @@ export const SliderFotosEstrutura = () => {
                     disableOnInteraction: false,
                 }}
             >
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1549675584-91f19337af3d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1472&q=80' /></SwiperSlide>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/espacoDeLeitura/espacoDeLeitura1.jpg?raw=true' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1588581939864-064d42ace7cd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1522211988038-6fcbb8c12c7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1585047402745-62c9c8cc3b89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80' /></SwiperSlide>
