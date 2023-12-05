835a5c1d31edc88949d3683ba2ee2805c17f14fe
Théo Barreto Silva <barretotheo25@gmail.com>
2023-12-05 00:37:24 -0300

    upload

--- src/components/SliderFotos/SliderFotos.tsx
+++ src/components/SliderFotos/SliderFotos.tsx
@@ -24,7 +24,7 @@ export const SliderFotos = () => {
             >
                 <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/frente_casa.jpg?raw=true' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1489171078254-c3365d6e359f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1631&q=80' /></SwiperSlide>
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1519643381401-22c77e60520e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1473&q=80' /></SwiperSlide>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/recepcao.jpg?raw=true' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1484154218962-a197022b5858?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1474&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1489769002049-ccd828976a6c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1661&q=80' /></SwiperSlide>
             </Swiper>
