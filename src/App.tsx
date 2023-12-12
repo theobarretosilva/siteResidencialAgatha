43bdee54e74cd1798f88c1843310075ee1b566db
Théo Barreto Silva <barretotheo25@gmail.com>
2023-12-12 19:04:41 -0300

    upload google tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import { Router } from './router/Router'
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: 'GTM-TJ5VDSLC'
+    }
+  
+    TagManager.initialize(tagManagerArgs);
+  }, [])
+
   return (
     <BrowserRouter>
       <Router />
