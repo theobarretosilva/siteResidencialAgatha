275228596c2fb2391679a98223631f8bf3895c04
Théo Barreto Silva <barretotheo25@gmail.com>
2023-12-12 10:33:05 -0300

    upload fotos jardim

--- src/components/SliderFotosEstrutura/SliderFotosEstrutura.tsx
+++ src/components/SliderFotosEstrutura/SliderFotosEstrutura.tsx
@@ -38,7 +38,7 @@ export const SliderFotosEstrutura = () => {
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1522211988038-6fcbb8c12c7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo-1585047402745-62c9c8cc3b89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80' /></SwiperSlide>
             </Swiper>
-            {/* <h1 className="titulo">Área Externa</h1>
+            <h1 className="titulo">Jardim</h1>
             <Swiper
                 slidesPerView={3}
                 modules={[Autoplay]}
@@ -48,12 +48,12 @@ export const SliderFotosEstrutura = () => {
                     disableOnInteraction: false,
                 }}
             >
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1585320806297-9794b3e4eeae?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80' /></SwiperSlide>
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1601654253194-260e0b6984f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1399&q=80' /></SwiperSlide>
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1550948390-6eb7fa773072?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1474&q=80' /></SwiperSlide>
-                <SwiperSlide><img src='https://images.unsplash.com/photo-1591383496652-db773e57b1d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' /></SwiperSlide>
-            </Swiper> */}
-            {/* <h1 className="titulo">Refeitório</h1>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/jardim/jardim1.jpg?raw=true' /></SwiperSlide>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/jardim/jardim2.jpg?raw=true' /></SwiperSlide>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/jardim/jardim3.jpg?raw=true' /></SwiperSlide>
+                <SwiperSlide><img src='https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/jardim/jardim4.jpg?raw=true' /></SwiperSlide>
+            </Swiper>
+            <h1 className="titulo">Refeitório</h1>
             <Swiper
                 slidesPerView={3}
                 modules={[Autoplay]}
@@ -67,7 +67,7 @@ export const SliderFotosEstrutura = () => {
                 <SwiperSlide><img src='https://images.unsplash&w=1399&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/-1550948390-6eb7fa773072?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1474&q=80' /></SwiperSlide>
                 <SwiperSlide><img src='https://images.unsplash.com/photo--db773e57b1d0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80' /></SwiperSlide>
-            </Swiper> */}
+            </Swiper>
         </>
     )
 }
