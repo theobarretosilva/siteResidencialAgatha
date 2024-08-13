c7c735164c206b295f3ba86cd226880c30342683
Théo Barreto Silva <barretotheo25@gmail.com>
2024-08-13 12:39:24 -0300

    fazendo tela de vantagens

--- src/pages/Vantagens/Vantagens.styles.ts
+++ src/pages/Vantagens/Vantagens.styles.ts
@@ -1,14 +1,59 @@
 import styled from "styled-components";
 
-export const DivFaixaDegrade = styled.div`
-    width: 100%;
-    height: 26vh;
-    background: linear-gradient(180deg, #383838 0%, rgba(56, 56, 56, 0.50) 100%);
-    margin-top: 14vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+export const ImgInicial = styled.img`
+    width: 99.67vw;
+    height: 64.45vh;
+    z-index: 0;
+    top: 14vh;
+    position: absolute;
+    object-fit: cover;
+
+    @media (max-width: 450px) {
+        height: 30vh;
+    }
+`
+
+export const SectionStyled = styled.section`
+    margin-top: 27.1vh;
+    z-index: 5;
+`
+
+export const TituloDivImgInicial = styled.h1`
+    color: #F8F4DE;
+    text-align: start;
+    font-family: 'Inter', sans-serif;
+    font-size: 3vw;
+    font-style: normal;
+    font-weight: 600;
+    margin-top: 7vh;
+    width: 65vw;
+    margin-bottom: 0;
+
+    @media (max-width: 450px) {
+        font-size: 9vw;
+        margin-top: 3vh;
+    }
+`
+
+export const TextoSection = styled.h1`
+    color: black;
+    font-size: 2.5rem;
+    font-style: normal;
+    font-weight: 600;
+    font-family: 'Inter', sans-serif;
+    text-align: center;
+    margin-top: 25vh;
+    margin-bottom: 0;
+
+    @media (max-width: 450px) {
+        font-size: 7.5vw;
+        margin-bottom: 2vh;
+    }
+`
+
+export const ImgSetaBaixo = styled.img`
+    height: 1.8vh;
+    margin-top: 1.5vh;
 `
 
 export const PDentroFaixa = styled.p`
@@ -25,77 +70,112 @@ export const PDentroFaixa = styled.p`
     }
 `
 
+export const ImgElipseFundo = styled.img`
+    width: 15vw;
+    margin-top: 160vh;
+    position: absolute;
+`
+
 export const MainBoxVantagens = styled.main`
     display: flex;
     flex-direction: row;
     flex-wrap: wrap;
     align-content: center;
-    justify-content: center;
-    margin-top: 5vh;
+    justify-content: space-between;
+    row-gap: 6vh;
+    margin-top: 8vh;
     margin-bottom: 5vh;
-
-    :nth-child(2){
-        border-left: none;
-    }
-    :nth-child(3){
-        border-right: none;
-        border-left: none;
-    }
-    :nth-child(5){
-        border-top: none;
-    }
-    :nth-child(6){
-        border-top: none;
-        border-left: none;
-    }
-    :nth-child(7){
-        border-top: none;
-        border-left: none;
-    }
+    z-index: 5;
 
     @media (max-width: 450px) {
         flex-direction: column;
-
-        :nth-child(2){
-            border-top: none;
-            border-bottom: none;
-            border-left: 2px solid #FFF;
-        }
-        :nth-child(3){
-            border-bottom: none;
-            border-left: 2px solid #FFF;
-            border-right: 2px solid #FFF;
-        }
-        :nth-child(5){
-            border-top: none;
-        }
-        :nth-child(6){
-            border-top: none;
-            border-left: 2px solid #FFF;
-        }
-        :nth-child(7){
-            border-top: none;
-            border-left: 2px solid #FFF;
-        }
     }
 `
 
-export const BoxVantagem = styled.section`
-    width: 18vw;
-    height: 26vh;
-    border: 2px solid #FFF;
+// export const MainBoxVantagens = styled.main`
+//     display: flex;
+//     flex-direction: row;
+//     flex-wrap: wrap;
+//     align-content: center;
+//     justify-content: center;
+//     margin-top: 10vh;
+//     margin-bottom: 5vh;
+
+//     :nth-child(2){
+//         border-left: none;
+//     }
+//     :nth-child(3){
+//         border-right: none;
+//         border-left: none;
+//     }
+//     :nth-child(5){
+//         border-top: none;
+//     }
+//     :nth-child(6){
+//         border-top: none;
+//         border-left: none;
+//     }
+//     :nth-child(7){
+//         border-top: none;
+//         border-left: none;
+//     }
+
+//     @media (max-width: 450px) {
+//         flex-direction: column;
+
+//         :nth-child(2){
+//             border-top: none;
+//             border-bottom: none;
+//             border-left: 2px solid #FFF;
+//         }
+//         :nth-child(3){
+//             border-bottom: none;
+//             border-left: 2px solid #FFF;
+//             border-right: 2px solid #FFF;
+//         }
+//         :nth-child(5){
+//             border-top: none;
+//         }
+//         :nth-child(6){
+//             border-top: none;
+//             border-left: 2px solid #FFF;
+//         }
+//         :nth-child(7){
+//             border-top: none;
+//             border-left: 2px solid #FFF;
+//         }
+//     }
+// `
+
+export const DivVantagem = styled.div`
     display: flex;
     flex-direction: column;
-    background-size: cover;
-    align-items: center;
+    justify-content: center;
+`
+
+export const ImgVantagem = styled.img`
+    border-top-left-radius: 0.6rem;
+    border-top-right-radius: 0.6rem;
+    width: 20.3vw;
+    height: 18vh;
+    object-fit: cover;
+`
+
+export const BoxVantagem = styled.section`
+    width: 20.2vw;
+    height: 18vh;
+    border-left: 0.64px solid #885F1A;
+    border-bottom: 0.64px solid #885F1A;
+    border-right: 0.64px solid #885F1A;
+    border-bottom-left-radius: 0.6rem;
+    border-bottom-right-radius: 0.6rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 
     @media (max-width: 450px) {
         width: 100%;
         max-height: 26vh;
-
-        :nth-child(2){
-            border: none;
-        }
     }
 `
 
@@ -118,12 +198,13 @@ export const IconBoxVantagem = styled.img`
 `
 
 export const TxtBoxVantagem = styled.h1`
-    color: #FFF;
-    font-size: 1.25vw;
-    font-style: normal;
+    color: #383838;
+    font-size: 1.15vw;
+    font-family: 'Inter', sans-serif;
     font-weight: 300;
     max-width: 17vw;
-    margin-top: 2vh;
+    text-align: start;
+    margin-left: 1vw;
 
     @media (max-width: 450px) {
         max-width: 90%;
--- src/pages/Vantagens/Vantagens.tsx
+++ src/pages/Vantagens/Vantagens.tsx
@@ -4,67 +4,59 @@ import * as S from './Vantagens.styles'
 export function Vantagens() {
     const advantagesData = [
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/acompanhamento_nutricional.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/1a559a11daa5cf6031ddbb843b27d9563622006e/src/assets/svg/icon_nutricionista.svg',
-            text: 'Acompanhamento nutricional',
+            backgroundImage: '../../src/assets/img/vantagens/acompanhamento_nutricional.jpg',
             secondText: 'Refeições inclusas e preparadas com carinho e acompanhamento nutricional',
         },
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/servicos_inclusos_na_mensalidade.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/1a559a11daa5cf6031ddbb843b27d9563622006e/src/assets/svg/icon_servicos_inclusos.svg',
-            text: 'Serviços inclusos na mensalidade',
-            secondText: 'Despesas fixas, como luz, água, gás, TV por assinatura, internet e telefone',
+            backgroundImage: '../../src/assets/img/vantagens/serviços_inclusos.jpg',
+            secondText: 'Despesas fixas, como luz, água, gás, internet e telefone',
         },
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/servicos_garantidos_por_nossa_equipe_multiprofissional.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/1a559a11daa5cf6031ddbb843b27d9563622006e/src/assets/svg/icon_servicos_garantidos.svg',
-            text: 'Serviços garantidos por nossa equipe multiprofissional',
+            backgroundImage: '../../src/assets/img/vantagens/serviços_garantidos.jpg',
             secondText: 'Limpeza da casa, lavanderia e serviços de segurança para a residência',
         },
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/equipe_especializada.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/1a559a11daa5cf6031ddbb843b27d9563622006e/src/assets/svg/icon_equipe_especializada.svg',
-            text: 'Equipe especializada',
+            backgroundImage: '../../src/assets/img/vantagens/equipe_especializada.jpg',
             secondText: 'Equipe especializada e preparada para cuidados com idosos',
         },
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/cuidados_com_medicacao.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/1a559a11daa5cf6031ddbb843b27d9563622006e/src/assets/svg/icon_cuidados_medicacao.svg',
-            text: 'Cuidados com medicação',
+            backgroundImage: '../../src/assets/img/vantagens/cuidados_com_medicacao.jpg',
             secondText: 'Cuidados com medicação e segurança vitalícia de idosos',
         },
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/esforcos_fisicos_e_emocionais_para_ajudar.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/1a559a11daa5cf6031ddbb843b27d9563622006e/src/assets/svg/icon_esforcos_fisicos.svg',
-            text: 'Esforços físicos e emocionais para ajudar',
+            backgroundImage: '../../src/assets/img/vantagens/esforcos_fisicos_para_ajudar.jpg',
             secondText: 'Esforços físicos e emocionais para ajudar nos cuidados diários',
         },
         {
-            backgroundImage: 'https://github.com/theobarretosilva/siteResidencialAgatha/blob/main/src/assets/img/atendimento_fundo.png?raw=true',
-            iconSrc: 'https://raw.githubusercontent.com/theobarretosilva/siteResidencialAgatha/3c8b668bafacb43b81b327d3ab98c6190155b15f/src/assets/svg/icon_atendimento.svg',
-            text: 'Atendimento',
+            backgroundImage: '../../src/assets/img/vantagens/esforcos_fisicos_para_ajudar.jpg',
             secondText: 'Recebemos residentes para centro dia e residência 24h'
+        },
+        {
+            backgroundImage: '../../src/assets/img/vantagens/app_ronda.jpg',
+            secondText: 'Texto sobre app de rondas noturnas'
         }
     ];
 
     return(
         <>
-            <S.DivFaixaDegrade>
-                <S.PDentroFaixa>Se você acredita que o Residencial Ágatha é a melhor escolha, mas tem dúvidas sobre o valor, veja as nossas vantagens:</S.PDentroFaixa>
-            </S.DivFaixaDegrade>
-            {/* ------------------------------------------------------------------------------------------------ */}
+            <S.ImgInicial src='../../src/assets/img/foto_pagina_vantagens.png' />
             <DivMargem>
+                <S.SectionStyled>
+                    <S.TituloDivImgInicial>Se você acredita que o Residencial Ágatha é a melhor escolha, mas tem dúvidas sobre o valor, veja as nossas vantagens:</S.TituloDivImgInicial>
+                </S.SectionStyled>
+                <S.TextoSection>Nossas vantagens</S.TextoSection>
+                <S.ImgSetaBaixo src='../../src/assets/svg/seta_baixo_preta.svg' />
+                <S.ImgElipseFundo style={{left: '0'}} src='../../src/assets/svg/elipse_fundo.svg' />
                 <S.MainBoxVantagens>
                     {advantagesData.map((advantage, index) => (
-                        <S.BoxVantagem
-                            key={index}
-                            style={{ backgroundImage: `url('${advantage.backgroundImage}')` }}
-                        >
-                            <S.IconBoxVantagem src={advantage.iconSrc} />
-                            <S.TxtBoxVantagem>
-                                {advantage.secondText}
-                            </S.TxtBoxVantagem>
-                        </S.BoxVantagem>
+                        <S.DivVantagem>
+                            <S.ImgVantagem src={advantage.backgroundImage} />
+                            <S.BoxVantagem key={index}>
+                                <S.TxtBoxVantagem>
+                                    {advantage.secondText}
+                                </S.TxtBoxVantagem>
+                            </S.BoxVantagem>
+                        </S.DivVantagem>
                     ))}
                 </S.MainBoxVantagens>
             </DivMargem>
